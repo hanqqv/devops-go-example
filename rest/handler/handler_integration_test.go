ba7d7e10333a5e20b82df3ebd569f9569c813eb0
vitsarut <han_f50@hotmail.com>
2024-05-09 21:51:46 +0700

    remove name after hello

--- rest/handler/handler_integration_test.go
+++ rest/handler/handler_integration_test.go
@@ -60,7 +60,7 @@ func TestITGetGreeting(t *testing.T) {
 	// Assertions
 	if assert.NoError(t, err) {
 		assert.Equal(t, http.StatusOK, resp.StatusCode)
-		assert.Equal(t, "Hello, qqv!", string(byteBody))
+		assert.Equal(t, "Hello!", string(byteBody))
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
